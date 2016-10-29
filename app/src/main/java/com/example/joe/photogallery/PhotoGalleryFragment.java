3805ea443523ba956715cb858f7c5452c39e75a8
Joe-Blake <wzj514332873@yahoo.com>
2016-10-30 00:49:12 +0800

    page need to debug

--- app/src/main/java/com/example/joe/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/joe/photogallery/PhotoGalleryFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,7 +56,6 @@ public class PhotoGalleryFragment extends Fragment {
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
                 if (!recyclerView.canScrollVertically(1)) {
-                    currentPage++;
                     new FetchItemsTask().execute(currentPage);
                 }
             }
